52e4bde59349e850fc8528ae08eb1c2904af851c
MomitaKundu <momita.kundu@bassetti-group.com>
2026-06-12 13:18:29 +0530

    Modify variable name

--- expense-tracker/ExpenseTracker.java
+++ expense-tracker/ExpenseTracker.java
@@ -108,12 +108,12 @@ public void ExpenseSearch(String category) {
     }
 }
 // Add Shopping Expense
-	public void shoppingexpense(String itemName, double price, int numberOfItem){
-        double total = price * numberOfItem;
+	public void shoppingexpense(String itemName, double itemPrice, int numberOfItem){
+        double totalPrice = ItemPrice * numberOfItem;
         System.out.println("Item name is :" +itemName);
-        System.out.println("Item price is :" +price);
-        System.out.println("Number of item is :" +itemName);
-        System.out.println("Total expenses for " +itemName+ " is :" +itemName);
+        System.out.println("Item price is :" +itemPrice);
+        System.out.println("Number of item is :" +numberOfItem);
+        System.out.println("Total expenses for " +itemName+ " is :" +totalPrice);
     }
 }
 }
