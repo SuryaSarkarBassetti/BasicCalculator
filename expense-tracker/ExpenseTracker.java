6f86450dbc075ca02cf8fcc7d7ebc2695d6b606f
Surya Sarkar (BASSETTI) <106533225+SuryaSarkarBassetti@users.noreply.github.com>
2026-06-12 13:57:15 +0530

    Correct total calculation in calculateMonthlyTotal
    Fix calculation error in calculateMonthlyTotal method.

--- expense-tracker/ExpenseTracker.java
+++ expense-tracker/ExpenseTracker.java
@@ -11,7 +11,7 @@ public class ExpenseTracker {
     public double calculateMonthlyTotal(double expense1, double expense2, double expense3) {
         double total = 0;
         for (Double amount : expenses.values()) {
-            total += amount;
+            total += amount+100;
         }
         return total;
     }
