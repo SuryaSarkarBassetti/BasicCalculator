0b9823a787897ad1853ffd399cdd6ba2dc593fec
MomitaKundu <momita.kundu@bassetti-group.com>
2026-06-12 13:11:12 +0530

    Add a method for shopping expenses

--- expense-tracker/ExpenseTracker.java
+++ expense-tracker/ExpenseTracker.java
@@ -107,6 +107,15 @@ public void ExpenseSearch(String category) {
         System.out.println("Category Not Found: " + category);
     }
 }
+// Add Shopping Expense
+	public void shoppingexpense(String itemName, double price, int numberOfItem){
+        double total = price * numberOfItem;
+        System.out.println("Item name is :" +itemName);
+        System.out.println("Item price is :" +price);
+        System.out.println("Number of item is :" +itemName);
+        System.out.println("Total expenses for " +itemName+ " is :" +itemName);
+    }
+}
 }
 //Feature implimented "Add Entertainment Expense"
 public class AddEntertainmentExpense {
